18686219ae85cf78452a0a43d87fba35f5ccde09
thetejasmagade <53045273+thetejasmagade@users.noreply.github.com>
2023-03-24 21:52:46 +0530

    feat: list all branches of repo

---
+++ server/routes/repos/[owner]/[repo]/branches/index.ts
@@ -0,0 +1,20 @@
+import type { GitHubBranches } from "~types";
+import { ghFetch } from "~/utils/github";
+
+export default eventHandler(async (event) => {
+  const repo = `${event.context.params.owner}/${event.context.params.repo}`;
+  const res = await ghFetch(`/repos/${repo}/branches`);
+
+  const branches = res.map(
+    (i) =>
+      <GitHubBranches>{
+        name: i.name,
+        commit: i.commit,
+        protected: i.protected,
+        protection: i.protction,
+        protection_url: i.protection_url
+      }
+  )
+
+  return { branches };
+});
