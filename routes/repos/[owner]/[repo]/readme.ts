96aec3bbafb64015f791e4e9e965237d7c68c4f1
Christian Preston <christianpreston@ymail.com>
2023-08-29 12:36:56 -0400

    fix(readme): rewrite relative urls with github cdn paths (#52)
    Co-authored-by: Pooya Parsa <pooya@pi0.io>

--- routes/repos/[owner]/[repo]/readme.ts
+++ routes/repos/[owner]/[repo]/readme.ts
@@ -1,16 +1,17 @@
+const GH_RAW_URL = "https://raw.githubusercontent.com";
+
 export default cachedEventHandler(
   async (event) => {
     const repo = `${event.context.params.owner}/${event.context.params.repo}`;
     const defaultBranch = await ghRepo(repo).then(
       (r) => r.default_branch || "main",
     );
-    const markdown = await $fetch<string>(
-      `https://raw.githubusercontent.com/${repo}/${defaultBranch}/README.md`,
-    );
+    const cdnBaseURL = `${GH_RAW_URL}/${repo}/${defaultBranch}`;
+    const markdown = await $fetch<string>(`${cdnBaseURL}/README.md`);
     const html = await ghMarkdown(markdown, repo, "readme");
     return {
-      html,
-      markdown,
+      markdown: resolveMarkdownRelativeLinks(markdown, cdnBaseURL),
+      html: resolveMarkdownRelativeLinks(html, cdnBaseURL),
     };
   },
   {
