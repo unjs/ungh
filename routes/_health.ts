7325635c4d4a45d840e6bd7db1461dfbf6c035cb
Pooya Parsa <pooya@pi0.io>
2023-11-20 13:33:05 +0100

    disable externed health checks for now

--- routes/_health.ts
+++ routes/_health.ts
@@ -1,15 +1,16 @@
 export default eventHandler(async () => {
-  const runtimeConfig = useRuntimeConfig();
-  const res = await $fetch.raw("/meta", {
-    baseURL: "https://api.github.com",
-    headers: {
-      "User-Agent": "fetch",
-      Authorization: "token " + runtimeConfig.GH_TOKEN,
-    },
-  });
-  const headers = Object.fromEntries(res.headers.entries());
-  return {
-    rateLimit:
-      headers["x-ratelimit-remaining"] + "/" + headers["x-ratelimit-limit"],
-  };
+  // const runtimeConfig = useRuntimeConfig();
+  // const res = await $fetch.raw("/meta", {
+  //   baseURL: "https://api.github.com",
+  //   headers: {
+  //     "User-Agent": "fetch",
+  //     Authorization: "token " + runtimeConfig.GH_TOKEN,
+  //   },
+  // });
+  // const headers = Object.fromEntries(res.headers.entries());
+  // return {
+  //   rateLimit:
+  //     headers["x-ratelimit-remaining"] + "/" + headers["x-ratelimit-limit"],
+  // };
+  return "OK";
 });
