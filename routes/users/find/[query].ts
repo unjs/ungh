f2a4e827f0723eb8eb7dc687f5e90d2bd8f16d0a
Pooya Parsa <pooya@pi0.io>
2024-03-06 16:48:03 +0100

    fix(user/find): match anonymous emails

--- routes/users/find/[query].ts
+++ routes/users/find/[query].ts
@@ -1,8 +1,17 @@
 import type { GithubUser } from "~types";
 
+const anonEmailRegex = /^\d+\+(.+)@users.noreply.github.com$/;
+
 export default eventHandler(async (event) => {
+  let query = event.context.params.query + '';
+
+  const anonMatch = query.match(anonEmailRegex);
+  if (anonMatch) {
+    query = anonMatch[1];
+  }
+
   const res = await ghFetch("/search/users", {
-    params: { q: event.context.params.query },
+    params: { q: query },
   });
 
   if (res.items.length === 0) {
