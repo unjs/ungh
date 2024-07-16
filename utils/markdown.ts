5f00ef5f0c140834cad2cbf6b4f9247950c3040d
Bobbie Goede <bobbiegoede@gmail.com>
2024-07-16 20:15:14 +0200

    fix: relative link replacement

--- utils/markdown.ts
+++ utils/markdown.ts
@@ -1,22 +1,30 @@
-const MARKDOWN_LINK_RE =
-  /(?<link>\[.*?]\((?<url>.*?)\)|<img.*?src="(?<url2>.*?)".*?>)/g;
+const HTML_LINK_RE = /(<img.*?src="(?<url2>.*?)".*?>)/g;
+const MARKDOWN_LINK_RE = /(?<link>\[.*?]\((?<url>.*?)\))/g;
 
 /**
- * HTML & Mardown Function to replace relative image paths with absolute paths
+ * Creates a string replacement function, prefixing matched strings with `baseURL`
+ */
+function createReplacerFn(baseURL: string) {
+  return (match, _, url: string | undefined, url2: string) => {
+    const path = url || url2;
+    // If path is already a URL, return the match
+    if (path.startsWith("http") || path.startsWith("https")) {
+      return match;
+    }
+    return match.replace(path, `${baseURL}/${path.replace(/^\.\//, "")}`);
+  };
+}
+
+/**
+ * HTML & Mardown Function to replace relative paths with absolute paths
+ * - prefixes relative image paths with `cdnBaseURL`
+ * - prefixes relative markdown paths with `ghBaseURL`
  */
 export function resolveMarkdownRelativeLinks(
   content: string,
-  cdnBaseURL: string,
+  options: { cdnBaseURL: string; githubBaseURL: string },
 ) {
-  return content.replace(
-    MARKDOWN_LINK_RE,
-    (match, _, url: string | undefined, url2: string) => {
-      const path = url || url2;
-      // If path is already a URL, return the match
-      if (path.startsWith("http") || path.startsWith("https")) {
-        return match;
-      }
-      return match.replace(path, `${cdnBaseURL}/${path.replace(/^\.\//, "")}`);
-    },
-  );
+  return content
+    .replace(HTML_LINK_RE, createReplacerFn(options.cdnBaseURL))
+    .replace(MARKDOWN_LINK_RE, createReplacerFn(options.githubBaseURL));
 }
