8e6311485e3f558f6a805dd39a2e018181d88fb6
IlyaL <ilyaliao324@gmail.com>
2026-03-21 15:01:25 +0800

    refactor: migrate user orgs to defineHandler and OpenAPI

--- routes/users/[name]/orgs.ts
+++ routes/users/[name]/orgs.ts
@@ -1,12 +1,27 @@
-import type { GithubOrg  } from "~types";
+import { defineRouteMeta, defineHandler } from "nitro";
+import { ghFetch } from "~/utils/github";
+import type { GithubOrg } from "~types";
 
-export default eventHandler(async (event) => {
-  const name = getRouterParam(event, "name");
+defineRouteMeta({
+  openAPI: {
+    description: "List GitHub organizations the user is a member of.",
+    parameters: [
+      {
+        name: "name",
+        in: "path",
+        required: true,
+        schema: { type: "string", example: "pi0" },
+      },
+    ],
+  },
+});
+
+export default defineHandler(async (event) => {
   // TODO: Do pagination
-  const rawOrgs = await ghFetch(`users/${name}/orgs?per_page=100`);
+  const rawOrgs = await ghFetch(`users/${event.context.params!.name}/orgs?per_page=100`);
 
   const orgs = rawOrgs.map(
-    (rawOrg) =>
+    (rawOrg: any) =>
       <GithubOrg>{
         id: rawOrg.id,
         name: rawOrg.login,
